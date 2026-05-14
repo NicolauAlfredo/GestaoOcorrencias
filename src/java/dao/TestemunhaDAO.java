3ad2f8e350eebd822143948e5e547339ead31c7b
Nicolau Alfredo <b.nicolaualfredo@gmail.com>
2026-05-14 19:48:59 +0100

    Refactor TestemunhaDAO with pagination, live search, and model alignment

--- src/java/dao/TestemunhaDAO.java
+++ src/java/dao/TestemunhaDAO.java
@@ -12,6 +12,8 @@ import java.sql.SQLException;
 import java.sql.Types;
 import java.util.ArrayList;
 import java.util.List;
+import modelo.Municipio;
+import modelo.Profissao;
 import modelo.Sexo;
 import modelo.Testemunha;
 import util.Conexao;
@@ -24,68 +26,138 @@ public class TestemunhaDAO implements GenericoDAO<Testemunha> {
 
     private static final int TOTAL_TESTEMUNHAS_POR_PAGINA = 6;
 
+    private static final String CAMPOS_CONSULTA
+            = "t.id_testemunha, "
+            + "t.nome_testemunha, "
+            + "t.pai_testemunha, "
+            + "t.mae_testemunha, "
+            + "t.bi_testemunha, "
+            + "t.residencia_testemunha, "
+            + "t.data_nascimento_testemunha, "
+            + "t.sexo_testemunha, "
+            + "t.proximidade_testemunha, "
+            + "t.estado_civil_testemunha, "
+            + "t.data_emissao_bi_testemunha, "
+            + "t.data_validade_bi_testemunha, "
+            + "t.telefone_testemunha, "
+            + "m.id_municipio, "
+            + "m.nome_municipio, "
+            + "p.id_profissao, "
+            + "p.nome_profissao";
+
+    private static final String BASE_SELECT
+            = "SELECT " + CAMPOS_CONSULTA + " "
+            + "FROM testemunha t "
+            + "INNER JOIN municipio m ON t.id_municipio = m.id_municipio "
+            + "INNER JOIN profissao p ON t.id_profissao = p.id_profissao ";
+
     private static final String INSERIR
             = "INSERT INTO testemunha "
-            + "(nome_testemunha, residencia_testemunha, data_nascimento_testemunha, "
-            + "sexo_testemunha, bi_testemunha, telefone_testemunha) "
-            + "VALUES (?, ?, ?, ?, ?, ?)";
+            + "(nome_testemunha, pai_testemunha, mae_testemunha, bi_testemunha, residencia_testemunha, "
+            + "data_nascimento_testemunha, sexo_testemunha, proximidade_testemunha, estado_civil_testemunha, "
+            + "data_emissao_bi_testemunha, data_validade_bi_testemunha, telefone_testemunha, "
+            + "id_municipio, id_profissao) "
+            + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
     private static final String ACTUALIZAR
             = "UPDATE testemunha SET "
             + "nome_testemunha = ?, "
+            + "pai_testemunha = ?, "
+            + "mae_testemunha = ?, "
+            + "bi_testemunha = ?, "
             + "residencia_testemunha = ?, "
             + "data_nascimento_testemunha = ?, "
             + "sexo_testemunha = ?, "
-            + "bi_testemunha = ?, "
-            + "telefone_testemunha = ? "
+            + "proximidade_testemunha = ?, "
+            + "estado_civil_testemunha = ?, "
+            + "data_emissao_bi_testemunha = ?, "
+            + "data_validade_bi_testemunha = ?, "
+            + "telefone_testemunha = ?, "
+            + "id_municipio = ?, "
+            + "id_profissao = ? "
             + "WHERE id_testemunha = ?";
 
     private static final String ELIMINAR
             = "DELETE FROM testemunha WHERE id_testemunha = ?";
 
     private static final String BUSCAR_POR_CODIGO
-            = "SELECT * FROM testemunha WHERE id_testemunha = ?";
+            = BASE_SELECT
+            + "WHERE t.id_testemunha = ?";
 
     private static final String LISTAR_TUDO
-            = "SELECT * FROM testemunha ORDER BY nome_testemunha";
+            = BASE_SELECT
+            + "ORDER BY t.nome_testemunha";
 
     private static final String LISTAR_POR_NOME
-            = "SELECT * FROM testemunha WHERE nome_testemunha LIKE ? ORDER BY nome_testemunha";
+            = BASE_SELECT
+            + "WHERE t.nome_testemunha LIKE ? "
+            + "ORDER BY t.nome_testemunha";
 
     private static final String LISTAR_POR_BI
-            = "SELECT * FROM testemunha WHERE bi_testemunha LIKE ? ORDER BY nome_testemunha";
+            = BASE_SELECT
+            + "WHERE t.bi_testemunha LIKE ? "
+            + "ORDER BY t.nome_testemunha";
 
     private static final String LISTAR_POR_DATA
-            = "SELECT * FROM testemunha WHERE data_nascimento_testemunha = ? ORDER BY nome_testemunha";
+            = BASE_SELECT
+            + "WHERE t.data_nascimento_testemunha = ? "
+            + "ORDER BY t.nome_testemunha";
 
     private static final String CONTAR_TESTEMUNHAS
             = "SELECT COUNT(1) AS total_testemunhas FROM testemunha";
 
     private static final String CONSULTAR_PAGINA
-            = "SELECT * FROM testemunha ORDER BY nome_testemunha LIMIT ? OFFSET ?";
+            = BASE_SELECT
+            + "ORDER BY t.nome_testemunha "
+            + "LIMIT ? OFFSET ?";
 
     private static final String CONTAR_TESTEMUNHAS_POR_NOME
-            = "SELECT COUNT(1) AS total_testemunhas FROM testemunha WHERE nome_testemunha LIKE ?";
+            = "SELECT COUNT(1) AS total_testemunhas "
+            + "FROM testemunha "
+            + "WHERE nome_testemunha LIKE ?";
 
     private static final String CONSULTAR_PAGINA_POR_NOME
-            = "SELECT * FROM testemunha WHERE nome_testemunha LIKE ? ORDER BY nome_testemunha LIMIT ? OFFSET ?";
+            = BASE_SELECT
+            + "WHERE t.nome_testemunha LIKE ? "
+            + "ORDER BY t.nome_testemunha "
+            + "LIMIT ? OFFSET ?";
 
     private static final String CONTAR_TESTEMUNHAS_POR_BI
-            = "SELECT COUNT(1) AS total_testemunhas FROM testemunha WHERE bi_testemunha LIKE ?";
+            = "SELECT COUNT(1) AS total_testemunhas "
+            + "FROM testemunha "
+            + "WHERE bi_testemunha LIKE ?";
 
     private static final String CONSULTAR_PAGINA_POR_BI
-            = "SELECT * FROM testemunha WHERE bi_testemunha LIKE ? ORDER BY nome_testemunha LIMIT ? OFFSET ?";
+            = BASE_SELECT
+            + "WHERE t.bi_testemunha LIKE ? "
+            + "ORDER BY t.nome_testemunha "
+            + "LIMIT ? OFFSET ?";
 
     private static final String CONTAR_TESTEMUNHAS_POR_DATA
-            = "SELECT COUNT(1) AS total_testemunhas FROM testemunha WHERE data_nascimento_testemunha = ?";
+            = "SELECT COUNT(1) AS total_testemunhas "
+            + "FROM testemunha "
+            + "WHERE data_nascimento_testemunha = ?";
 
     private static final String CONSULTAR_PAGINA_POR_DATA
-            = "SELECT * FROM testemunha WHERE data_nascimento_testemunha = ? ORDER BY nome_testemunha LIMIT ? OFFSET ?";
+            = BASE_SELECT
+            + "WHERE t.data_nascimento_testemunha = ? "
+            + "ORDER BY t.nome_testemunha "
+            + "LIMIT ? OFFSET ?";
 
     @Override
     public void save(Testemunha testemunha) {
         if (testemunha == null) {
             System.err.println("Erro ao INSERIR testemunha: o objeto testemunha não pode ser nulo.");
+            return;
+        }
+
+        if (testemunha.getMunicipio() == null) {
+            System.err.println("Erro ao INSERIR testemunha: o município não pode ser nulo.");
+            return;
+        }
+
+        if (testemunha.getProfissao() == null) {
+            System.err.println("Erro ao INSERIR testemunha: a profissão não pode ser nula.");
             return;
         }
 
@@ -111,6 +183,16 @@ public class TestemunhaDAO implements GenericoDAO<Testemunha> {
     public void update(Testemunha testemunha) {
         if (testemunha == null) {
             System.err.println("Erro ao ACTUALIZAR testemunha: o objeto testemunha não pode ser nulo.");
+            return;
+        }
+
+        if (testemunha.getMunicipio() == null) {
+            System.err.println("Erro ao ACTUALIZAR testemunha: o município não pode ser nulo.");
+            return;
+        }
+
+        if (testemunha.getProfissao() == null) {
+            System.err.println("Erro ao ACTUALIZAR testemunha: a profissão não pode ser nula.");
             return;
         }
 
@@ -308,11 +390,7 @@ public class TestemunhaDAO implements GenericoDAO<Testemunha> {
         }
 
         int pagina = converterNumeroPagina(numeroPagina);
-        int offset = (pagina * TOTAL_TESTEMUNHAS_POR_PAGINA) - TOTAL_TESTEMUNHAS_POR_PAGINA;
-
-        if (offset < 0) {
-            offset = 0;
-        }
+        int offset = calcularOffset(pagina);
 
         try {
             conn = Conexao.getConnection();
@@ -391,11 +469,7 @@ public class TestemunhaDAO implements GenericoDAO<Testemunha> {
         List<Testemunha> testemunhas = new ArrayList<Testemunha>();
 
         int pagina = converterNumeroPagina(numeroPagina);
-        int offset = (pagina * TOTAL_TESTEMUNHAS_POR_PAGINA) - TOTAL_TESTEMUNHAS_POR_PAGINA;
-
-        if (offset < 0) {
-            offset = 0;
-        }
+        int offset = calcularOffset(pagina);
 
         try {
             conn = Conexao.getConnection();
@@ -430,11 +504,7 @@ public class TestemunhaDAO implements GenericoDAO<Testemunha> {
         List<Testemunha> testemunhas = new ArrayList<Testemunha>();
 
         int pagina = converterNumeroPagina(numeroPagina);
-        int offset = (pagina * TOTAL_TESTEMUNHAS_POR_PAGINA) - TOTAL_TESTEMUNHAS_POR_PAGINA;
-
-        if (offset < 0) {
-            offset = 0;
-        }
+        int offset = calcularOffset(pagina);
 
         try {
             conn = Conexao.getConnection();
@@ -541,6 +611,16 @@ public class TestemunhaDAO implements GenericoDAO<Testemunha> {
         }
     }
 
+    private int calcularOffset(int pagina) {
+        int offset = (pagina * TOTAL_TESTEMUNHAS_POR_PAGINA) - TOTAL_TESTEMUNHAS_POR_PAGINA;
+
+        if (offset < 0) {
+            return 0;
+        }
+
+        return offset;
+    }
+
     private void preencherPreparedStatement(
             PreparedStatement ps,
             Testemunha testemunha,
@@ -548,36 +628,66 @@ public class TestemunhaDAO implements GenericoDAO<Testemunha> {
     ) throws SQLException {
 
         ps.setString(1, testemunha.getNomeTestemunha());
-        ps.setString(2, testemunha.getResidenciaTestemunha());
+        ps.setString(2, testemunha.getPaiTestemunha());
+        ps.setString(3, testemunha.getMaeTestemunha());
+        ps.setString(4, testemunha.getBiTestemunha());
+        ps.setString(5, testemunha.getResidenciaTestemunha());
 
-        if (testemunha.getDataNascimentoTestemunha() != null) {
-            ps.setDate(3, new java.sql.Date(testemunha.getDataNascimentoTestemunha().getTime()));
-        } else {
-            ps.setNull(3, Types.DATE);
-        }
+        setDate(ps, 6, testemunha.getDataNascimentoTestemunha());
 
         if (testemunha.getSexo() != null) {
-            ps.setString(4, testemunha.getSexo().getExtensao());
+            ps.setString(7, testemunha.getSexo().getExtensao());
         } else {
-            ps.setNull(4, Types.VARCHAR);
+            ps.setNull(7, Types.VARCHAR);
         }
 
-        ps.setString(5, testemunha.getBiTestemunha());
-        ps.setString(6, testemunha.getTelefoneTestemunha());
+        ps.setString(8, testemunha.getProximidadeTestemunha());
+        ps.setString(9, testemunha.getEstadoCivilTestemunha());
+
+        setDate(ps, 10, testemunha.getDataEmissaoBiTestemunha());
+        setDate(ps, 11, testemunha.getDataValidadeBiTestemunha());
+
+        ps.setString(12, testemunha.getTelefoneTestemunha());
+        ps.setInt(13, testemunha.getMunicipio().getIdMunicipio());
+        ps.setInt(14, testemunha.getProfissao().getIdProfissao());
 
         if (actualizar) {
-            ps.setInt(7, testemunha.getIdTestemunha());
+            ps.setInt(15, testemunha.getIdTestemunha());
+        }
+    }
+
+    private void setDate(PreparedStatement ps, int index, java.util.Date data) throws SQLException {
+        if (data != null) {
+            ps.setDate(index, new java.sql.Date(data.getTime()));
+        } else {
+            ps.setNull(index, Types.DATE);
         }
     }
 
     private void popularComDados(Testemunha testemunha, ResultSet rs) throws SQLException {
         testemunha.setIdTestemunha(rs.getInt("id_testemunha"));
         testemunha.setNomeTestemunha(rs.getString("nome_testemunha"));
+        testemunha.setPaiTestemunha(rs.getString("pai_testemunha"));
+        testemunha.setMaeTestemunha(rs.getString("mae_testemunha"));
+        testemunha.setBiTestemunha(rs.getString("bi_testemunha"));
         testemunha.setResidenciaTestemunha(rs.getString("residencia_testemunha"));
         testemunha.setDataNascimentoTestemunha(rs.getDate("data_nascimento_testemunha"));
         testemunha.setSexo(Sexo.getExtensao(rs.getString("sexo_testemunha")));
-        testemunha.setBiTestemunha(rs.getString("bi_testemunha"));
+        testemunha.setProximidadeTestemunha(rs.getString("proximidade_testemunha"));
+        testemunha.setEstadoCivilTestemunha(rs.getString("estado_civil_testemunha"));
+        testemunha.setDataEmissaoBiTestemunha(rs.getDate("data_emissao_bi_testemunha"));
+        testemunha.setDataValidadeBiTestemunha(rs.getDate("data_validade_bi_testemunha"));
         testemunha.setTelefoneTestemunha(rs.getString("telefone_testemunha"));
+
+        Municipio municipio = new Municipio();
+        municipio.setIdMunicipio(rs.getInt("id_municipio"));
+        municipio.setNomeMunicipio(rs.getString("nome_municipio"));
+        testemunha.setMunicipio(municipio);
+
+        Profissao profissao = new Profissao();
+        profissao.setIdProfissao(rs.getInt("id_profissao"));
+        profissao.setNomeProfissao(rs.getString("nome_profissao"));
+        testemunha.setProfissao(profissao);
     }
 
     private void fecharResultSet(ResultSet rs) {
